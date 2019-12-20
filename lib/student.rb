2ca6e391e9f3f4729f22a9077d9db691342da81e
Randy Steele <steelerx155@gmail.com>
2019-12-20 22:11:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,7 +15,8 @@ class Student < InteractiveRecord
       column_names << column["name"]
       end
     column_names.compact
-  end  
+   
+  
   self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
   end
