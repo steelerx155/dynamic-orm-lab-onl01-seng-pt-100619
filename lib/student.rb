59c685271c2a4c93535396f4cd987faaa1f09233
Randy Steele <steelerx155@gmail.com>
2019-12-20 18:52:04 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,28 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
+
+  def initialize(options={})
+    options.each do |property, value|
+    self.send("#{property}=", value)
+    end
+  end
+
+
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+  
+  def self.column_names
+    DB[:conn].results_as_hash = true
+    
+    sql ="PRAGMA table_info('#{table_name}')"
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+    
+    table_info.each do |column|
+      column_names << column["name"]
+    end
+    column_names.compact
+  end  
 end
