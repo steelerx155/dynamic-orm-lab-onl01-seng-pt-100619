402ba29be8275782e4a8d88b684ff800a53f1730
Randy Steele <steelerx155@gmail.com>
2019-12-20 22:14:43 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -53,4 +53,7 @@ def self.find_by_name(name)
   DB[:conn].execute(sql, name)
 end
 
+def self.find_by(options)
+  sql = "SELECT * FROM #{self.table_name} WHERE options = ?"
+  DB[:conn].execute(sql, options)
 end
